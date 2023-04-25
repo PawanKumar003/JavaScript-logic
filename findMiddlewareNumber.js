ae136ec6ffe530006884f1e03754030522444989
Pawan <pk06181@gmail.com>
2023-04-25 20:44:53 +0530

    find middleware number from array and pettern

---
+++ findMiddlewareNumber.js
@@ -0,0 +1,7 @@
+// find middleware number from array
+
+const arr = [2, 5, 7, 8, 9, 90, 5, 5, 6, 7, 9];
+
+const length = arr.length;
+const index = Math.floor(arr.length / 2);
+console.log(arr[index]);
---
+++ starPettern.js
@@ -0,0 +1,8 @@
+// use for loop method for star pettern
+
+for (let i = 1; i <= 5; i++) {
+  for (let k = 1; k <= i; k++) {
+    document.write(k);
+  }
+  document.write("<br>");
+}
